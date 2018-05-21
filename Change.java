c644c0c2935ab880f5ed69ef37d9f32d96e208eb
Miłosz Janowski <milekjan@gmail.com>
2018-05-21 16:26:27 +0200

    logger added Change.toString() added

--- Change.java
+++ Change.java
@@ -1,9 +1,17 @@
-import java.sql.Date;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class Change {
+    private int ID;
     private Date date;
     private List<LineChange> changeList;
+
+    public Change(int ID) {
+        this.ID = ID;
+        this.date = new Date();
+        this.changeList = Collections.emptyList();
+    }
 
     public Date getDate() {
         return date;
@@ -17,5 +25,17 @@ public class Change {
         return ID;
     }
 
-    private int ID;
+    @Override
+    public String toString() {
+        StringBuilder changeString = new StringBuilder();
+        for (LineChange c: changeList) {
+            changeString.append(c.toString());
+            changeString.append("\n");
+        }
+
+        return "Change ID: " + ID  + "\n"
+                + "Change date: " + date + "\n"
+                + changeString;
+
+    }
 }
